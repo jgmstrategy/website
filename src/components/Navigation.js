36691d961b147d0a337582b471e502797991d794
Gideon Tong <gideon@gideontong.com>
2023-03-06 23:07:37 -0800

    Add navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,28 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navigation() {
   return (
-    <Navbar></Navbar>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">JGM Consulting</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Industries</Nav.Link>
+            <Nav.Link href="#home">Services</Nav.Link>
+            <Nav.Link href="#home">Insights</Nav.Link>
+            <Nav.Link href="#home">About Us</Nav.Link>
+            <Nav.Link href="#home">Careers</Nav.Link>
+            <Nav.Link href="#home">Blog</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import { createRoot } from 'react-dom';
 import Button from 'react-bootstrap/Button';
+
+import Navigation from './components/Navigation';
 
 const App = () => (
   <div>
+    <Navigation></Navigation>
     <div class="container py-4 px-3 mx-auto">
       <h1>JGM Consulting</h1>
       <Button variant="primary">Begin</Button>
@@ -12,4 +14,5 @@ const App = () => (
   </div>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
