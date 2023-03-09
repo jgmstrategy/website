b5ed16ec04a1a83fedd3d0707868f414777e07ce
Gideon Tong <gideon@gideontong.com>
2023-03-09 15:34:39 -0800

    Create about images

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import {
+  MDBRow,
+  MDBCol,
+} from 'mdb-react-ui-kit';
+
+import nyc from '../images/nyc.jpg';
+import gideon from '../images/gideon.jpg';
+import maxwell from '../images/maxwell.jpg';
+import jason from '../images/jason.jpg';
 
 function About() {
   return (
-    <div className='container py-4 px-3 mx-auto'>
-      <p>Coming soon</p>
+    <div>
+      <div className='text-center bg-image'>
+        <img src={nyc} className='img-fluid shadow-4' />
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 className='mb-3'>Our Amazing Team</h1>
+              <h4 className='mb-3'>Here at JGM Strategy, we have great minds navigating the world's complex problems.</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='container py-4 px-3 mx-auto'>
+        <MDBRow>
+          <MDBCol lg='4' md='4' className='mb-4'>
+            <img src={gideon} className='img-fluid rounded-circle'/>
+          </MDBCol>
+          <MDBCol lg='4' md='4' className='mb-4'>
+            <img src={maxwell} className='img-fluid rounded-circle'/>
+          </MDBCol>
+          <MDBCol lg='4' md='4' className='mb-4'>
+            <img src={jason} className='img-fluid rounded-circle'/>
+          </MDBCol>
+        </MDBRow>
+      </div>
     </div>
   );
 }
