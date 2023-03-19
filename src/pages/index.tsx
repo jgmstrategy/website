c26c24975cad6b2f07c587d6e6950dcd4f314c19
Gideon Tong <gideon@gideontong.com>
2023-03-18 17:52:34 -0700

    Scaffold page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -57,6 +58,9 @@ export default function Home() {
               priority
             />
           </div>
+          <h1 className="title">
+            Read <Link href="/posts/first-post">this page!</Link>
+          </h1>
         </div>
 
         <div className={styles.grid}>
@@ -119,5 +123,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
