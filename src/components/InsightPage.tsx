e7230b9e4c48ed8b31b3952e3ebc7fa2b762a32e
Gideon Tong <gideon@gideontong.com>
2023-03-21 13:59:47 -0700

    Insight page started

---
+++ src/components/InsightPage.tsx
@@ -0,0 +1,65 @@
+import { PropsWithChildren } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+type InsightPageProps = {
+  title: string;
+  image: string;
+  description: string;
+  subheader: string;
+}
+
+export default function InsightPage(props: PropsWithChildren<InsightPageProps>) {
+  return (
+    <>
+      <Head>
+        <title>
+          {props.title} | JGM Strategy Consulting
+        </title>
+        <meta name='description' content={props.description} />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <div className='bg-image' style={{ height: '25rem', overflow: 'visible' }}>
+        <Image
+          src={props.image}
+          alt=''
+          style={{ objectFit: 'cover' }}
+          fill
+        />
+        <div className='mask' style={{ overflow: 'visible' }}>
+          <div
+            className='d-flex align-items-end justify-content-center h-100 py-4'
+            style={{ marginTop: '5rem' }}
+          >
+            <Paper elevation={6} sx={{ bottom: 0 }}>
+              <Card sx={{ minWidth: 275, bottom: 0 }}>
+                <CardContent>
+                  <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
+                    {props.subheader}
+                  </Typography>
+                  <Typography variant='h5' component='div' className='pb-3'>
+                    {props.title}
+                  </Typography>
+                  <Typography variant='body1'>
+                    <b>{props.description}</b>
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Paper>
+          </div>
+        </div>
+      </div>
+      <Container maxWidth='md' sx={{ paddingTop: '6rem', paddingBottom: '2rem' }}>
+        <p>Authors list</p>
+        <p>Date of publication</p>
+        <p>Share button coming soon</p>
+        {props.children}
+      </Container>
+    </>
+  );
+}
--- src/pages/insights/better-patient-experiences.tsx
+++ src/pages/insights/better-patient-experiences.tsx
@@ -1,3 +1,14 @@
+import InsightPage from '../../components/InsightPage';
+
 export default function BetterPatientExperiences() {
-  return <h1>First Post</h1>;
+  return (
+    <InsightPage
+      title='Improving the patient experience at the dentist&apos;s office with WiFi'
+      image='/insights/better-patient-experiences.jpg'
+      description='Keeping the stress levels at an all-time low'
+      subheader='Healthcare Insight'
+    >
+      <p>Content</p>
+    </InsightPage>
+  );
 }
