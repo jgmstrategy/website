75971ad7c1119817cf1a3fb18416dbefa2d56395
Gideon Tong <gideon@gideontong.com>
2023-03-21 00:01:59 -0700

    Add some placeholders

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,6 +8,13 @@ export default function Contact() {
         <meta name='description' content='' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
+      <p>Grid</p>
+      <p>Contact us for business</p>
+      <p>Contact form</p>
+      <p>Press box</p>
+      <p>Not business? Want career instead? Go to careers page box</p>
+      <p>Social media</p>
+      <p>Alumni relations box</p>
     </>
   );
 }
--- src/pages/insights/index.tsx
+++ src/pages/insights/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Container from '@mui/material/Container';
 
 export default function Insights() {
   return (
@@ -8,6 +9,13 @@ export default function Insights() {
         <meta name='description' content='' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
+      <Container>
+        <p>Placeholder for featured insights</p>
+        <p>Placeholder for recommended insights</p>
+        <p>Random feature of some annual overview</p>
+        <p>Carousel placeholder</p>
+        <p>Grid of recent insights</p>
+      </Container>
     </>
   );
 }
