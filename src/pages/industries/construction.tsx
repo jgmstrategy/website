eccd085adcf75588e8cba0d1aa819a2dda7172ed
Maxwell Chang <machang@ucsd.edu>
2023-04-12 18:38:12 -0700

    [CONTENT] Added description for construction industry

--- src/pages/industries/construction.tsx
+++ src/pages/industries/construction.tsx
@@ -6,7 +6,9 @@ function ConstructionDescription() {
   return (
     <>
       <Typography>
-        We hope to have a description of our industry and services we offer here soon.
+        The construction industry is in the midst of significant transformation, driven by technological advancements and shifts in customer demands. To stay competitive, companies must differentiate themselves by adopting new digital technologies, rethinking their traditional value chain activities, and investing in customer-centric strategies.
+
+        At our company, we work closely with construction companies, real estate developers, architects, and engineering firms to help them navigate this dynamic landscape. From modernizing construction sites with smart technologies and robotics to implementing sustainable building practices and enhancing customer experiences, we partner with our clients to develop innovative solutions that enable them to achieve their business goals. By leveraging our expertise in analytics, automation, and digital media, we help construction companies optimize their operations, reduce costs, and create more efficient and sustainable buildings.
       </Typography>
     </>
   );
