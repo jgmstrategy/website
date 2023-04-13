f83d8e9101f131afd21a21eb87bd034ec7ed0165
Gideon Tong <gideon@gideontong.com>
2023-04-13 13:57:36 -0700

    Fix linting errors for education

--- src/pages/industries/education.tsx
+++ src/pages/industries/education.tsx
@@ -6,7 +6,16 @@ function EducationDescription() {
   return (
     <>
       <Typography>
-        Our team of experts in the education industry uses data analytics, cutting-edge technologies, and innovative methodologies to create custom solutions that cater to the unique needs of each client. By leveraging these tools, we help education organizations streamline their operations, enhance student engagement and learning outcomes, and stay ahead of the curve in a rapidly evolving landscape. Our approach is grounded in collaboration, as we work closely with our clients to understand their specific challenges and identify opportunities for improvement. Through this partnership, we enable education providers to unlock their full potential and prepare students for success in an ever-changing world.
+        Our team of experts in the education industry uses data analytics,
+        cutting-edge technologies, and innovative methodologies to create custom
+        solutions that cater to the unique needs of each client. By leveraging
+        these tools, we help education organizations streamline their operations,
+        enhance student engagement and learning outcomes, and stay ahead of the
+        curve in a rapidly evolving landscape. Our approach is grounded in
+        collaboration, as we work closely with our clients to understand their
+        specific challenges and identify opportunities for improvement. Through
+        this partnership, we enable education providers to unlock their full
+        potential and prepare students for success in an ever-changing world.
       </Typography>
     </>
   );
