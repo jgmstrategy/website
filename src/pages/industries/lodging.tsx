74d9cb2a8f2a6ce92592d2c1afd8cc577762b6a1
Gideon Tong <gideon@gideontong.com>
2023-04-13 14:38:01 -0700

    Fix all linting errors

--- src/pages/industries/lodging.tsx
+++ src/pages/industries/lodging.tsx
@@ -1,18 +1,36 @@
-import Typography from '@mui/material/Typography';
+import {
+  Stack,
+  Typography
+} from '@mui/material';
 
 import IndustryPage from '@/components/IndustryPage';
 
 function LodgingAccommodationDescription() {
   return (
-    <>
+    <Stack direction='column' spacing={2}>
       <Typography>
-        The lodging and accommodation industry is an essential part of the global economy, providing travelers with a wide range of options for their stays, including hotels, motels, resorts, bed and breakfasts, and vacation rentals. To succeed in this highly competitive market, companies must adapt to changing consumer preferences and technological advancements.
-
-        At our company, we help lodging and accommodation businesses transform their operations and improve the customer experience by leveraging cutting-edge technology and innovative solutions. Our services include digital marketing, e-commerce, data analytics, supply chain optimization, and operational efficiency improvements.
-
-        We work closely with our clients to identify their unique challenges and opportunities, developing customized strategies that enable them to remain competitive and profitable in an ever-evolving marketplace. Whether it's improving their online presence, streamlining their supply chain, or enhancing the guest experience, our experts are dedicated to helping lodging and accommodation companies achieve success.
+        The lodging and accommodation industry is an essential part of the global
+        economy, providing travelers with a wide range of options for their
+        stays, including hotels, motels, resorts, bed and breakfasts, and
+        vacation rentals. To succeed in this highly competitive market, companies
+        must adapt to changing consumer preferences and technological advancements.
       </Typography>
-    </>
+      <Typography>
+        At our company, we help lodging and accommodation businesses transform
+        their operations and improve the customer experience by leveraging
+        cutting-edge technology and innovative solutions. Our services include
+        digital marketing, e-commerce, data analytics, supply chain optimization,
+        and operational efficiency improvements.
+      </Typography>
+      <Typography>
+        We work closely with our clients to identify their unique challenges and
+        opportunities, developing customized strategies that enable them to
+        remain competitive and profitable in an ever-evolving marketplace.
+        Whether it&apos;s improving their online presence, streamlining their supply
+        chain, or enhancing the guest experience, our experts are dedicated to
+        helping lodging and accommodation companies achieve success.
+      </Typography>
+    </Stack>
   );
 }
 
--- src/pages/industries/retail.tsx
+++ src/pages/industries/retail.tsx
@@ -6,7 +6,16 @@ function RetailDescription() {
   return (
     <>
       <Typography>
-        The retail industry is undergoing a rapid transformation as new technologies and changing consumer behaviors are disrupting traditional business models. At our company, we partner with retailers to help them navigate these challenges and capitalize on new opportunities. We work closely with our clients to develop innovative strategies that leverage the latest technologies such as e-commerce platforms, digital marketing, and analytics to create more personalized shopping experiences for their customers. Our approach is to not just provide short-term solutions, but to work with retailers to transform their business models, build customer loyalty, and position them for long-term success in a rapidly changing retail landscape.
+        The retail industry is undergoing a rapid transformation as new
+        technologies and changing consumer behaviors are disrupting traditional
+        business models. At our company, we partner with retailers to help them
+        navigate these challenges and capitalize on new opportunities. We work
+        closely with our clients to develop innovative strategies that leverage
+        the latest technologies such as e-commerce platforms, digital marketing,
+        and analytics to create more personalized shopping experiences for their
+        customers. Our approach is to not just provide short-term solutions, but
+        to work with retailers to transform their business models, build customer
+        loyalty, and position them for long-term success in a rapidly changing retail landscape.
       </Typography>
     </>
   );
